6157ccd35df757c42419b04cbd382bed2d14c8dd
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-12-27 12:43:37 +0800

    Compile libldacBT_bco.so

--- common.mk
+++ common.mk
@@ -102,6 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
